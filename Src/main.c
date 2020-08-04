24f37973fd36fc4f8eb460c1ea628dcb7d1f7d8b
stancecoke <hochsitzcola@web.de>
2020-08-04 19:41:28 +0200

    Set UART BAUD rate to 115200

--- Src/main.c
+++ Src/main.c
@@ -179,7 +179,7 @@ int main(void)
 
 	  if(UART_RX_Flag){
 		  UART_RX_Flag=0;
-
+		  HAL_GPIO_TogglePin(Onboard_LED_GPIO_Port, Onboard_LED_Pin);
 #if (DISPLAY_TYPE == DISPLAY_TYPE_DEBUG)
 		  if(UART_TX_Flag){
 			 UART_TX_Flag=0;
@@ -493,7 +493,7 @@ static void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
-  huart1.Init.BaudRate = 9600;
+  huart1.Init.BaudRate = 115200;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
